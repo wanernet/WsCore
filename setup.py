bcdc35fc2b6dde42f8e377505e618e13b0565e07
Waner <wanernet@qq.com>
2015-06-13 22:07:11 +0800

    version Create

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='WsCore',
+    version='0.0.1',
+    packages=[''],
+    url='http://www.wanersoft.com/python',
+    license='',
+    author='Waner',
+    author_email='wanernet@qq.com',
+    description=''
+)
